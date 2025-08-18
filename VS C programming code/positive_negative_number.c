1809dd28b0bf414413e3232ca6e283969e10becb
Bhawika Kumari <bhawika3103@gmail.com>
2025-08-18 14:02:17 +0530

    C Program

---
+++ VS C programming code/positive_negative_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int X;
+    printf("Enter x value:");
+    scanf("%d",&X);
+    if(X>0)
+    {
+        printf("Positive\n");
+    }
+    else
+    {
+        printf("Negative\n");
+    }
+    return 0;
+
+}
